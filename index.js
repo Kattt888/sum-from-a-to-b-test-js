8768443a431a60e0ab6138c88fef3344bff6e1ed
Katrina <katrinabyrd88@gmail.com>
2024-10-14 01:02:49 -0400

    completed the index.js recursion activity

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN === toN) { // setting up fromN to equal toN
+    return toN; //setting up base case - returning the value when fromN equals toN
+  }
+  return fromN + sum(fromN + 1, toN); //recursive case adding fromN and calling sum plus the next num
 }
 
 module.exports = sum;
